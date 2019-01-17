6565b0b4afa85c654223d346cc04d51b9bc801d0
Pranay Kumar <pranay.kumar@gatech.edu>
2019-01-17 02:37:44 -0500

    - added Firebase core functionality - added Firebase authentication functionality - added ability to sign up for an account and have it stored in Firebase - added login authentication - added pod file

--- CHaRM/CHaRM/LoginViewController.swift
+++ CHaRM/CHaRM/LoginViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +20,21 @@ class LoginViewController: UIViewController {
     }
     
 
+    @IBAction func loginAction(_ sender: Any) {
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!) { (user, error) in
+            if error == nil{
+                self.performSegue(withIdentifier: "loginToHome", sender: self)
+            }
+            else{
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+        
+    }
     /*
     // MARK: - Navigation
 
