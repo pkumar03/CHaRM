4ec9ecca6cf916ea809cf229ccb0a9344652c33e
Adya Modi <adyamodi@samanthas-macbook-air-3.local>
2019-01-23 22:13:38 -0500

    colors to every screen

--- CHaRM/CHaRM/FourthViewController.swift
+++ CHaRM/CHaRM/FourthViewController.swift
@@ -12,7 +12,7 @@ class FourthViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor=#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         // Do any additional setup after loading the view.
     }
     
--- CHaRM/CHaRM/SignUpViewController.swift
+++ CHaRM/CHaRM/SignUpViewController.swift
@@ -16,7 +16,7 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var passwordConfirm: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor=#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
         // Do any additional setup after loading the view.
     }
     
