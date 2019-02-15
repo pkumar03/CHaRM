cd09f2197a5f5afcaccbd8ae1a412e3cb4fa201c
Pranay Kumar <pranay.kumar@gatech.edu>
2019-02-15 13:43:17 -0500

    - added the ability to store multiple materials at once into the Firebase database by using a semaphore + multithreading. Removed debugging statements and fixed some typos.

--- CHaRM/CHaRM/StoriesTableViewController.swift
+++ CHaRM/CHaRM/StoriesTableViewController.swift
@@ -1,10 +1,6 @@
 //
 //  StoriesTableViewController.swift
 //  CHaRM
-//
-//  Created by Annette Cochran on 1/16/19.
-//  Copyright © 2019 JID8306. All rights reserved.
-//
 
 import UIKit
 import FirebaseDatabase
@@ -40,9 +36,6 @@ class StoriesTableViewController: UITableViewController {
         //I added a semaphore to control access to currentMaterial + made a new dispatch queue and used gcd to control execution of the threads
         
         let queue = DispatchQueue(label: "com.gcd.myQueue", attributes: .concurrent)
-        var time  = 0
-        print("time at start of for loop: ", time)
-        time = time + 1
         let semaphore = DispatchSemaphore(value: 1)
 
         for i in ourArray {
@@ -51,36 +44,18 @@ class StoriesTableViewController: UITableViewController {
             
                 semaphore.wait()
                 self.currentMaterial = i
-
-            
-                //self.currentMaterial = i
-                print("current material is currently: ", self.currentMaterial)
-                print("time is now at: ", time)
-                time = time + 1
-        
-                print("time at entry of problematic code: ", time)
+                
                 _ = Database.database().reference().root.child("Materials Brought").child(self.currentMaterial).observeSingleEvent(of: .value) { (DataSnapshot) in
                     let valToUpdate = DataSnapshot.value as? NSNumber
-                    print ("time when we get valToUpdate is & for item: ", time, self.currentMaterial)
-                    time = time + 1
-                    print("value to update is: ", valToUpdate?.intValue as Any)
                     self.updatedInt = (valToUpdate?.intValue)! //aborts if value is nil fix during code review if needed also hi
-                    print("Reached group leave statement")
-                //semaphore.signal()
                 }
             
-                //look into putting a mutex lock on currentMaterial
-                print("time before dispatch queue is: ", time)
-                time = time + 1
+                //look into putting a semaphore lock on currentMaterial
+              
+                
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                    print("now inside dispatch queue at time: ", time)
-                    time = time + 1
-                    print("updated int before increment is:", self.updatedInt)
                     self.updatedInt = self.updatedInt + 1
-                    print("updated int is now:, ", self.updatedInt)
                     _ = Database.database().reference().root.child("Materials Brought").child(self.currentMaterial).setValue(self.updatedInt)
-                    print("time after new int is in database is: ", time)
-                    time = time + 1
                     semaphore.signal()
                 }
             }
@@ -127,7 +102,7 @@ class StoriesTableViewController: UITableViewController {
         Recyclable(id: 10, section: 0, title: "Bulbs", text: ""),
         Recyclable(id: 11, section: 0, title: "CFL Bulbs", text: ""),
         Recyclable(id: 12, section: 0, title: "Toilets", text: "Seats and all hardware must be removed")],
-        [Recyclable(id: 13, section: 1, title: "Plastics/clean, dry, empty plastic bags/ film packaging/grocery bags", text: ""),
+        [Recyclable(id: 13, section: 1, title: "Plastic or grocery bags", text: "Plastics/clean, dry, empty plastic bags/ film packaging/grocery bags"),
         Recyclable(id: 14, section: 1, title: "Plastic food containers", text: "#1-#2-#5, clean and dry"),
         Recyclable(id: 15, section: 1, title: "Paper", text: "magazines/office paper/newspaper/phone books"),
         Recyclable(id: 16, section: 1, title: "Glass bottles and jars", text: "food grade only"),
@@ -138,13 +113,13 @@ class StoriesTableViewController: UITableViewController {
         Recyclable(id: 21, section: 1, title: "Textiles", text: ""),
         Recyclable(id: 22, section: 1, title: "Styrofoam", text: "please wash"),
         Recyclable(id: 23, section: 1, title: "Batteries", text: "Zinc Carbon, Button, Lithium, Rechargeable, Car"),
-        Recyclable(id: 24, section: 1, title: "Household fats/oil/grease", text: ""),
+        Recyclable(id: 24, section: 1, title: "Household fats or oil or grease", text: ""),
         Recyclable(id: 25, section: 1, title: "Waxed Cartons", text: "Broth, Milk, Juice"),
         Recyclable(id: 26, section: 1, title: "Wine Corks", text: ""),
         Recyclable(id: 27, section: 1, title: "Political signs", text: "")],
         [Recyclable(id: 28, section: 2, title: "Books", text: ""),
         Recyclable(id: 29, section: 2, title: "Musical Instruments", text: ""),
-        Recyclable(id: 30, section: 2, title: "Sports equiptment", text: ""),
+        Recyclable(id: 30, section: 2, title: "Sports equipment", text: ""),
         Recyclable(id: 31, section: 2, title: "Bikes", text: ""),
         Recyclable(id: 32, section: 2, title: "Textiles", text: ""),
         Recyclable(id: 33, section: 2, title: "Furniture in usable condition", text: "No Office Furniture"),
