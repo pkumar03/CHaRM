25d9cd80c166b4b4abea93bc99d45a060ab92bbe
Pranay Kumar <pranay.kumar@gatech.edu>
2019-03-07 12:58:36 -0500

    - recycling history is now displayed from profile page - PayPal embedded into app using button on donate page

--- CHaRM/CHaRM/SecondViewController.swift
+++ CHaRM/CHaRM/SecondViewController.swift
@@ -7,9 +7,24 @@
 //
 
 import UIKit
+import SafariServices
 
 class SecondViewController: UIViewController {
 
+    @IBAction func payPalPressed(_ sender: Any) {
+        showSafariVC(for: "https://www.paypal.com/donate/?token=gu_ENMnbElN5hZ2eWQeAgtjcaKatUHbA_bpGSBgz1ptShHN7St4EoZuS93Fr5x5pZI4QhW&country.x=US&locale.x=US")
+        
+    }
+    
+    func showSafariVC(for url:String) {
+        guard let url = URL(string: url) else {
+            //show an invaild URL error alert
+            return
+        }
+        
+        let safariVC = SFSafariViewController(url: url)
+        present(safariVC, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
